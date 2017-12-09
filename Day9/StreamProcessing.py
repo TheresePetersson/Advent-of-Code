e46b4382689a89131840c0a570a58539d6ceb5d3
TheresePetersson <therese-petersson@live.se>
2017-12-09 14:05:35 +0100

    Day 9 Complete

---
+++ Day9/StreamProcessing.py
@@ -0,0 +1,34 @@
+# Task 1 and Task 2
+score = 0
+f = open('input.txt','r')
+lst = f.read().strip()
+ignore_next = False
+garbage = False
+nestled = 0
+removed = 0
+for char in lst:
+	if garbage:
+		removed += 1
+	if not ignore_next:
+		if char == "<":
+			garbage = True
+		elif char == ">":
+			garbage = False
+			removed -=1
+		elif char == "{" and not garbage:
+			nestled += 1
+		elif char == "}" and not garbage:
+			score += nestled
+			nestled -= 1
+		elif char == "!":
+			ignore_next = True
+			if garbage:
+				removed -= 1
+	else:
+		ignore_next = False
+		if garbage:
+			removed -= 1
+
+print "Task 1: " + str(score)
+print "Task 2: " + str(removed)
+
